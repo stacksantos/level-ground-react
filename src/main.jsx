7a9a7cc4a4dcce36c5c2d5219c19ad622560e40b
stacksantos <stacksantos@proton.me>
2022-09-02 18:59:22 -0500

    update routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+	<HashRouter>
+		<Routes>
+			<Route path='/' element={<App />} />
+		</Routes>
+	</HashRouter>
+);
